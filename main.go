e3bf0b683560f94daeb3dff1152ce2dd2839af27
davejbax <davejbax@gmail.com>
2025-04-03 22:59:00 +0100

    Add plugin version to fix packer-sdc plugin check

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/davejbax/packer-plugin-arm/builder"
+	"github.com/davejbax/packer-plugin-arm/version"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"log"
 )
@@ -9,6 +10,7 @@ import (
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder(plugin.DEFAULT_NAME, builder.NewBuilder())
+	pps.SetVersion(version.PluginVersion)
 	if err := pps.Run(); err != nil {
 		log.Fatal(err.Error())
 	}
